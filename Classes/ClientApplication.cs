d36f1a2a38e076c4dfa0ffff470607c212827642
unknown <madysoftware@gmail.com>
2020-04-22 23:26:16 +0500

    Добавлена функция подавки заявки

--- Classes/ClientApplication.cs
+++ Classes/ClientApplication.cs
@@ -15,8 +15,8 @@ namespace Bank_Credit_Manager
         public void CreateApplication(string _name, string _gender, string _isMarried, int _age, string _nation, int _creditSumm, string _creditAim, string _creditTerm)
         {
             SQLManager _sqlManager = new SQLManager();
-            _sqlManager.InsertData("users_application", "_name, _user_gender, _user_age, _married, _nationality, _credit_summ_from_general_revenue, _credit_history, _arrearage_in_credit_history, _credit_aim, _credit_term, _status, _ball, _results, _is_payed", $"_name, _user_gender, _user_age, _married, _nationality, _credit_summ_from_general_revenue, _credit_history, {this.CreditArrearage()}, {_creditAim}, {_creditTerm}, 'NONE', 0, 0, 0");
-            
+            _sqlManager.InsertData("users_application", "_name, _user_gender, _user_age, _married, _nationality, _credit_summ_from_general_revenue, _credit_history, _arrearage_in_credit_history, _credit_aim, _credit_term, _status, _ball, _results, _is_payed", 
+            $"{_name}, {_gender}, {_age}, {_isMarried}, {_nation}, {_creditSumm}, {this.CreditsCount()}, {this.CreditArrearage()}, {_creditAim}, {_creditTerm}, 'NONE', 0, 0, 0");
         }
 
         public int CreditArrearage()
