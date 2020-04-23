ca438cc83f9f934555ff5101074e26a60e4ccf15
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-04-23 19:50:21 +0300

    Изменения в классе ClientApplication

--- Classes/ClientApplication.cs
+++ Classes/ClientApplication.cs
@@ -12,19 +12,19 @@ namespace Bank_Credit_Manager
 {
     public class ClientApplication : IClientApplication
     {
-        
-        public ClientApplication(string name)
+        private stringc _name;
+        public ClientApplication(string clientName)
         {
-
+            _name = clientName;
         }
-        public void CreateApplication(string _name, string _gender, string _isMarried, int _age, string _nation, int _creditSumm, string _creditAim, string _creditTerm)
+        public void CreateApplication(string _gender, string _isMarried, int _age, string _nation, int _creditSumm, string _creditAim, string _creditTerm)
         {
             SQLManager _sqlManager = new SQLManager();
             _sqlManager.InsertData("users_application", "_name, _user_gender, _user_age, _married, _nationality, _credit_summ_from_general_revenue, _credit_history, _arrearage_in_credit_history, _credit_aim, _credit_term, _status, _ball, _results, _is_payed", 
             $"'{_name}', '{_gender}', {_age}, '{_isMarried}', '{_nation}', {_creditSumm}, {this.CreditsCount(_name)}, {this.CreditArrearage(_name)}, '{_creditAim}', {_creditTerm}, 'NONE', 0, 0, 0");
         }
 
-        public int CreditArrearage(string _name)
+        public int CreditArrearage()
         {
             SQLManager _sqlManager = new SQLManager();
             SqlDataReader _reader = _sqlManager.Select($"select _name from users_application where _name='{_name}'");
@@ -37,7 +37,7 @@ namespace Bank_Credit_Manager
                 return 0;
         }
 
-        public int CreditsCount(string _name)
+        public int CreditsCount()
         {
             SQLManager _sqlManager = new SQLManager();
             SqlDataReader _reader = _sqlManager.Select($"select _name from users_application where _name='{_name}'");
--- Interfaces/IClientApplication.cs
+++ Interfaces/IClientApplication.cs
@@ -2,8 +2,9 @@ namespace Bank_Credit_Manager
 {
     interface IClientApplication
     {
-        void CreateApplication(string _name, string _gender, string _isMarried, int _age, string _nation, int _creditSumm, string _creditAim, string _creditTerm);
-        int CreditsCount(string _name);
-        int CreditArrearage(string _name);
+        void CreateApplication(string _gender, string _isMarried, int _age, string _nation, int _creditSumm, string _creditAim, string _creditTerm);
+        int CreditsCount();
+        int CreditArrearage();
+        bool AcceptedToCredit();
     }
 }
