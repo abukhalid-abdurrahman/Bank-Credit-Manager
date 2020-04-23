c31561ecfa0ba136ab08d69016e819dcf24de6ed
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-04-23 22:43:59 +0300

    Класс для работы с пользователем

---
+++ Classes/UserInterface.cs
@@ -0,0 +1,16 @@
+/*
+    Разработчик: _faha_
+    Дата: 23.04.2020
+    Класс: UserInterface
+    Файл: UserInterface.cs
+    Описание: Пользовательский интерфейс
+*/
+using System;
+
+namespace Bank_Credit_Manager
+{
+    public class UserInterface : IUserInterface
+    {
+
+    }
+}
---
+++ Interfaces/IUserInterface.cs
@@ -0,0 +1,7 @@
+namespace Bank_Credit_Manager
+{
+    public class IUserInterface
+    {
+        
+    }
+}
