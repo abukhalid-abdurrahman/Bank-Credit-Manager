3ae53a7c8c955995f955d198e9de7c80c5cd27c9
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-04-24 05:37:57 +0300

    Весь функционал прописан

--- Program.cs
+++ Program.cs
@@ -6,6 +6,31 @@ namespace Bank_Credit_Manager
     {
         static void Main(string[] args)
         {
+            UserInterface UI = new UserInterface();
+            UI.Output("Здравствуйте! Вас приветствует банк Faridun Сармой 2020! Хотите получить кредит за 5 минут? Подайте заявку!!!");
+            UI.Output("Выберите что хотите сделать: ");
+            UI.Output("1. Войти");
+            UI.Output("2. Регистрация");
+            UI.Output("3. Внести деньги");
+            UI.Output("0. Закрыть программу");
+            string cmd = UI.Input("Сделайте выбор(1,2): ");
+            while(cmd != "0")
+            {
+                if(cmd == "1")
+                {
+                    UI.LoginOutput();
+                }
+                else if(cmd == "2")
+                {
+                    UI.RegistrateOutput();
+                }
+                else if(cmd == "3")
+                {
+                    float _login = UI.Input("Введите логин(номер телефона): ");
+                    float _summ = UI.Input("Введите сумму: ");
+                    UI.PaymentStory(_summ, _login);
+                }
+            }
         }
     }
 }
